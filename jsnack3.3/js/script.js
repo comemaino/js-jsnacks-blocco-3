36a523a35d0eb4b271b01495eba62f5a0d676717
comemaino <mat.maino@gmail.com>
2022-05-04 16:59:20 +0200

    Add: jsnack3.3 script

--- jsnack3.3/js/script.js
+++ jsnack3.3/js/script.js
@@ -1,1 +1,32 @@
 // 3. Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0. Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti.
+
+const teams = [
+  {
+    name: "Milan",
+    goal: 0,
+    falli: 0,
+  },
+  {
+    name: "Inter",
+    goal: 0,
+    falli: 0,
+  },
+  {
+    name: "Atalanta",
+    goal: 0,
+    falli: 0,
+  },
+];
+
+for (let i = 0; i < teams.length; i++) {
+  const thisElement = teams[i];
+  if (thisElement.goal === 0) {
+    const rndNumber = Math.floor(Math.random() * 100);
+    thisElement.goal = rndNumber;
+  }
+  if (thisElement.falli === 0) {
+    const rndNumber = Math.floor(Math.random() * 100);
+    thisElement.falli = rndNumber;
+    console.log(thisElement);
+  }
+}
