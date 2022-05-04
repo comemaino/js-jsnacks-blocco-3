a1e10491e7f361a57ec9e30ea59be250a4595cda
comemaino <mat.maino@gmail.com>
2022-05-04 15:13:14 +0200

    jsnack 3.1 Init

---
+++ jsnack3.1/js/script.js
@@ -0,0 +1,5 @@
+// 1. Crea due array che hanno un numero di elementi diversi. Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
+
+//creo due array
+const arr1 = [1, 2, 3, 4, 5, 6];
+const arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
